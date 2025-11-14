0f893649832ab3fe052c44ef83ebf5ec61f5245a
Min <jeon320@naver.com>
2025-11-14 21:26:57 +0900

    2025-11-14 갤러리쇼핑몰 FE 레이아웃 완성, 회원가입 페이지 완성

--- gallery-shopingmall/gallery/frontend/src/components/Header.vue
+++ gallery-shopingmall/gallery/frontend/src/components/Header.vue
@@ -1,3 +1,4 @@
+import router from '../router';
 <template>
   <header>
     <div class="navbar navbar-dark bg-dark text-white shadow-sm">
@@ -6,7 +7,7 @@
           <strong>Gallery</strong>
         </router-link>
         <div class="menus d-flex gap-3">
-          <template v-if="true">
+          <template v-if="!accountStore.loggedIn">
             <router-link to="/login">로그인 </router-link>
             <router-link to="/join">회원가입</router-link>
           </template>
@@ -22,9 +23,23 @@
 </template>
 
 <script setup>
+import { logout } from '@/services/accountService';
+import { useAccountStore } from '@/stores/account';
+import { useRouter } from 'vue-router';
+
+
+//계정 스토어
+const accountStore= useAccountStore();
+//라우터 객체
+const router = useRouter();
 //로그아웃
 const logoutAccount= async ()=>{
-  window.alert("준비 중입니다.");
+    const res = await logout();
+
+    if(res.status === 200){
+      accountStore.setLoggedIn(false);
+      await router.push("/");
+    }
 }
 </script>
 
--- gallery-shopingmall/gallery/frontend/src/router/index.js
+++ gallery-shopingmall/gallery/frontend/src/router/index.js
@@ -8,6 +8,11 @@ const router = createRouter({
       name: 'home',
       component: ()=>import('../views/Home.vue')
     },
+    {
+      path: '/join',
+      name: 'join',
+      component: ()=>import('../views/Join.vue')
+    },
 
 
   ],
